655620bf33f7d098e80af912bef04fb4c36dc0c3
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-02-02 13:36:06 +0100

    Add frontend input binding to core routes

--- easypharmacy-frontend/src/app/app.config.ts
+++ easypharmacy-frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
@@ -7,6 +7,6 @@ import { JwtInterceptor } from './auth/interceptor/JwtInterceptor';
 import { employeeRoutes } from './employee/employee.module';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideRouter(employeeRoutes),provideHttpClient(withInterceptorsFromDi()),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withComponentInputBinding()),provideRouter(employeeRoutes),provideHttpClient(withInterceptorsFromDi()),
     {provide: HTTP_INTERCEPTORS,useClass:JwtInterceptor,multi:true}]
 };
